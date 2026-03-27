64cd28d2936bf3f5e842ad773c0b562d1d4d731c
AllMighty-57 <ecainepetty@gmail.com>
2026-03-27 15:59:25 -0700

    Edit particles

---
+++ Tweet Tweet!/Assets/Scripts/ParticlePosition.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class ParticlePosition : MonoBehaviour
+{
+    public GameObject monster;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = monster.GetComponent<Rigidbody2D>();   
+    }
+
+    private void Update()
+    { 
+        Vector2 currentMonster = rb.transform.position;
+        Vector2 currentParticle = transform.position;
+
+        if (currentParticle != currentMonster)
+        {
+            transform.position = currentMonster; 
+            Debug.Log("The particle is at the monsters position.");
+        }
+    }
+}
