a94967646cb550916664cd2c601c87aea5679cff
AllMighty-57 <ecainepetty@gmail.com>
2026-04-10 14:06:05 -0700

    fixed again again

--- Tweet Tweet!/Assets/Scripts/PauseMenu.cs
+++ Tweet Tweet!/Assets/Scripts/PauseMenu.cs
@@ -43,7 +43,7 @@ public class PauseMenu : MonoBehaviour
     } 
     public void QuitGame()
     {
-        SceneManager.LoadScene("Credit");
+        SceneManager.LoadScene("Credits");
         Debug.Log("Get outta HERE!");
     } 
 
